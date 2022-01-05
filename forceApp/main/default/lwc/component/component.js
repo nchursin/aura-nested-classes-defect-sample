93f1e0978a3697c9e97195a4a38ef2c22d25a42d
Nikita Chursin <chursinn91@gmail.com>
2022-01-05 13:41:50 +0300

    fixed

--- forceApp/main/default/lwc/component/component.js
+++ forceApp/main/default/lwc/component/component.js
@@ -1,37 +1,41 @@
-import { LightningElement } from 'lwc';
-import { ShowToastEvent } from 'lightning/platformShowToastEvent'
-import doSomething from '@salesforce/apex/Controller.doSomething';
+import { LightningElement } from "lwc";
+import { ShowToastEvent } from "lightning/platformShowToastEvent";
+import doSomething from "@salesforce/apex/Controller.doSomething";
 
 export default class SampleComponent extends LightningElement {
-    _rendered = false;
+  _rendered = false;
 
-    async renderedCallback() {
-        if (!this._rendered) {
-            doSomething(
-                {
-                    request: {
-                        name: 'name'
-                    }
-                })
-                .then(result => {
-                    this.showToast('SUCCESS', 'Works!')
-                })
-                .catch(error => {
-                    console.error(error)
-                    this.showToast('error', 'Doesn\'t work!')
-                });
-            this._rendered = true;
+  async renderedCallback() {
+    if (!this._rendered) {
+      const resourceRequest = {
+        request: {
+          name: "name"
         }
-    }
+      };
 
-    showToast(variant, title, message) {
-        const event = new ShowToastEvent({
-            title,
-            message,
-            variant,
-            mode: 'sticky'
+      doSomething({
+        resourceRequest
+      })
+        .then((result) => {
+          console.log("result.result >> ", result);
+          this.showToast("SUCCESS", `Works! Result: ${result.result}`);
+        })
+        .catch((error) => {
+          console.error(error);
+          this.showToast("error", "Doesn't work!");
         });
-  
-        this.dispatchEvent(event);
+      this._rendered = true;
     }
+  }
+
+  showToast(variant, title, message) {
+    const event = new ShowToastEvent({
+      title,
+      message,
+      variant,
+      mode: "sticky"
+    });
+
+    this.dispatchEvent(event);
+  }
 }
